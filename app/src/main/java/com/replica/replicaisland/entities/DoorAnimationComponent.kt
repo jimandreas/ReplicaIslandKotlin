8b3e78258495911c3b10e2e47957029cb9161017
Jim <jim.r.andreas@gmail.com>
2025-11-17 07:33:34 +0100

    lint pass 10

--- app/src/main/java/com/replica/replicaisland/entities/DoorAnimationComponent.kt
+++ app/src/main/java/com/replica/replicaisland/entities/DoorAnimationComponent.kt
@@ -14,6 +14,8 @@
  * limitations under the License.
  */
 
+@file:Suppress("unused", "unused", "unused", "unused")
+
 package com.replica.replicaisland.entities
 
 import com.replica.replicaisland.mechanics.ChannelSystem
